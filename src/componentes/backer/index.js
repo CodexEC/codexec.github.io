dd56228dbc89b24e72557d967792aecc68102ef1
Vladimir Rodríguez <fizzvr@gmail.com>
2018-08-08 16:57:30 -0500

    Se implementa un baker de fondo q debera unirse a parallax > Desarrollador 🔋
    Desarrollo Web NodeJS • Quito Ecuador
    
    [Fix: ]

---
+++ src/componentes/backer/index.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./efectos.css";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const parallaxStyle = {
+  parallax: {
+    position: "absolute",
+    height: "100vh",
+    width: "100%",
+    margin: "0",
+    padding: "0",
+    overflow: "hidden",
+    listStyleType: "none",
+    zIndex: "-1"
+  },
+  imagenEstilo: {
+    position: "absolute",
+    left: "0",
+    top: "0",
+    width: "100%",
+    height: "100%",
+    opacity: "0",
+    zIndex: "1",
+    padding: "0",
+    margin: "0",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  }
+};
+
+class Backer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imagenes: props.imagenes,
+      efectos: ["fade", "right", "bottom"],
+      currentSlide: 0
+    };
+    this.runSlideShow = this.runSlideShow.bind(this);
+    this.autoSlideshow = this.autoSlideshow.bind(this);
+  }
+  componentDidMount() {
+    this.runSlideShow();
+  }
+
+  runSlideShow() {
+    setInterval(this.autoSlideshow, 3500);
+  }
+
+  autoSlideshow() {
+    this.setState({
+      currentSlide: (this.state.currentSlide + 1) % this.state.imagenes.length
+    });
+    console.log(this.state.currentSlide);
+  }
+
+  render() {
+    const { classes, imagenes, children } = this.props;
+    const { efectos, currentSlide } = this.state;
+
+    const slideShowSlides = imagenes.map((slide, i) => {
+      return (
+        <li
+          className={`slide ${efectos[i]} ${
+            currentSlide === i ? "showing-" + efectos[i] : "no-pongas"
+          }`}
+          key={i}
+          style={{ backgroundImage: `url(${slide})` }}
+        />
+      );
+    });
+
+    return (
+      <div
+        style={{
+          position: "absolute",
+          height: "100%",
+          width: "100%"
+        }}
+        >
+        <ul className={classes.parallax}>{slideShowSlides}</ul>
+        {children}
+      </div>
+    );
+  }
+}
+
+Backer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  imagenes: PropTypes.array,
+  children: PropTypes.node
+};
+
+export default withStyles(parallaxStyle)(Backer);
--- src/contenedores/PrivacidadPagina/index.js
+++ src/contenedores/PrivacidadPagina/index.js
@@ -3,9 +3,14 @@ import { Helmet } from "react-helmet";
 import plantilla from "../../plantilla/index";
 import Contador from "../../componentes/contador";
 import Tiempo from "../../componentes/tiempo";
+import Backer from "../../componentes/backer";
+
+import A from "../../act/img/0.jpg";
+import B from "../../act/img/1.jpg";
+import C from "../../act/img/2.jpg";
 
 var start = new Date().getTime();
-console.log(start);
+
 
 const Index = () => {
   return (
@@ -13,10 +18,12 @@ const Index = () => {
       <Helmet>
         <title>Privacidad</title>
       </Helmet>
-      <div>Privacidad</div>
-      <h1>Priva:</h1>
-      <Tiempo elapsed={new Date().getTime() - start} />
-      <Contador />
+      <Backer imagenes={[A, B, C]}>
+        <div>Privacidad</div>
+        <h1>Priva:</h1>
+        <Tiempo elapsed={new Date().getTime() - start} />
+        <Contador />
+      </Backer>
     </div>
   );
 };
