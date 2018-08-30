60343aaa4aac74b6bf87dd7dfc85036843e11037
Vladimir Rodríguez <fizzvr@gmail.com>
2018-08-29 21:56:51 -0500

    Prevent local request to Analitycs > Desarrollador 🔋
    Desarrollo Web NodeJS • Quito Ecuador
    
    [Fix: ]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./codexec.css";
 import App from "./App.codexec";
+import registerServiceWorker from "./registerServiceWorker";
 
 console.log("====================================");
 console.log("> BETA.ini");
 ReactDOM.render(<App />, document.getElementById("codexec"));
+registerServiceWorker();
 console.log("> BETA.fin");
 console.log("====================================");
